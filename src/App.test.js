625fc7625c49444d5332e703efa89d8c111f1e05
limitlesscar <rentalcarapplimitlessslimitles@gmail.com>
2026-06-15 21:26:38 +0200

    new
    new

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+jest.mock('./firebase', () => ({
+  dbFirebase: {
+    collection: jest.fn(() => ({
+      add: jest.fn(),
+      where: jest.fn(() => ({
+        get: jest.fn(() => Promise.resolve({ forEach: jest.fn() })),
+      })),
+    })),
+  },
+  auth: {},
+  firestore: {},
+  storage: {},
+}));
+
+test('renders the website home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/rent a car worldwide/i)).toBeInTheDocument();
+  expect(screen.getByText(/select a city/i)).toBeInTheDocument();
+  expect(screen.getAllByRole('link', { name: /hosting/i }).length).toBeGreaterThan(0);
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,25 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import { TextDecoder, TextEncoder } from 'util';
 import '@testing-library/jest-dom';
+import './i18n';
+
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+window.scrollTo = jest.fn();
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  configurable: true,
+  value: query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
